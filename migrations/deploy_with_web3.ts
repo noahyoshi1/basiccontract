ea386f5a462fbee2a772882a24086abe2c22d9f7
noahyoshi1 <127420816+noahyoshi1@users.noreply.github.com>
2023-03-27 19:54:14 -0400

    Update deploy_with_web3.ts

--- migrations/deploy_with_web3.ts
+++ migrations/deploy_with_web3.ts
@@ -1,9 +1,10 @@
 // This script can be used to deploy the "Storage" contract using ethers.js library.
 // Please make sure to compile "./contracts/1_Storage.sol" file before running this script.
 // And use Right click -> "Run" from context menu of the file to run the script. Shortcut: Ctrl+Shift+S
-require('dotenv').config()
-
+import { ethers } from 'ethers';
 import { deploy } from './ethers-lib'
+
+require('dotenv').config();
 
 const privateKey = process.env.PRIVATE_KEY;
 const providerUrl = proccess.env.PROVIDER_URL;
