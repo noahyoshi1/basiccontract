b7637aa12c031c0c02ff8148bf0f1c60572d3fc3
noahyoshi1 <127420816+noahyoshi1@users.noreply.github.com>
2023-03-27 20:05:48 -0400

    Update deploy_with_web3.ts

--- migrations/deploy_with_web3.ts
+++ migrations/deploy_with_web3.ts
@@ -1,22 +1,10 @@
-// This script can be used to deploy the "Storage" contract using ethers.js library.
-// Please make sure to compile "./contracts/1_Storage.sol" file before running this script.
-// And use Right click -> "Run" from context menu of the file to run the script. Shortcut: Ctrl+Shift+S
-import { ethers } from 'ethers';
-import { deploy } from './ethers-lib'
-
-require('dotenv').config();
-
-const privateKey = process.env.PRIVATE_KEY;
-const providerUrl = proccess.env.PROVIDER_URL;
-
-const provider = new ethers.providers.JsonRpcProvider(providerUrl);
-const signer = new Wallet(privateKey, provider);
+import { deploy } from './web3-lib'
 
 (async () => {
     try {
-        const result = await deploy('Storage', [], signer)
+        const result = await deploy('Storage', [])
         console.log(`address: ${result.address}`)
     } catch (e) {
         console.log(e.message)
     }
-  })()
+})()
